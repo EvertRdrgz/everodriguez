1a2438d64317ca4bc8ab425a1f6807f64aaa0fe2
Evert Rodriguez <everodriguez@csumb.edu>
2018-03-04 07:44:18 +0000

    labs 3 and 4

---
+++ labs/lab3/index.php
@@ -0,0 +1,227 @@
+<?php
+	//Varaible for holding time
+    $startTime  = microtime(true);
+	//Start session
+    session_start(); //start or resume a session
+	//Check if there is a session
+    if (!isset($_SESSION['gameCount'])) { 
+        $_SESSION['gameCount'] = 1;
+        $_SESSION['finalTime'] = 0;
+    }
+    // Returns an array of cards per player
+    function retrieveHand(&$cards)
+    {
+         // Stores the hand of cards for player in an array
+        $playerHand = array();
+        $playerScore = 0;
+        // Drawing another card if haven’t reached score
+        while($playerScore <= 36)
+        {
+            $drawCard = rand(1, 13);
+            $drawSuit = rand(0, 3);
+            while($cards[13 * $drawSuit + $drawCard] == 1)
+            {
+                $drawCard = rand(1,13);
+                $drawSuit = rand(0,3);
+            }
+        
+            $cards[13 * $drawSuit + $drawCard] = 1;
+            switch ($drawSuit) 
+            {
+                case 0: $drawSuit = "spades";
+                    break;
+                case 1: $drawSuit = "diamonds";
+                    break;
+                case 2: $drawSuit = "clubs";
+                    break;
+                case 3: $drawSuit = "hearts";
+                    break;
+            }
+            
+            $playerHand[] = $drawSuit;
+            $playerHand[] = $drawCard;
+            $playerScore = $playerScore + $drawCard;
+            $i++;
+        }
+        $playerHand[] = $playerScore;
+        return $playerHand;
+    }
+    
+    function retrievePlayer($character, $total) 
+    {
+        echo "<img id='players' src=img/players/$character[$total].png>";
+        echo "<h3>$character[$total]</h3>";
+        return $character;
+    }
+    
+    // Displays the array of cards per player along with the sum of points
+    function showPlayerCards($playerHand)
+    {
+        for($i=0; $i<count($playerHand) - 1; $i++) 
+        {
+            $temp = $i + 1;
+            echo "<img id='playerHand' src=cards/$playerHand[$i]/$playerHand[$temp].png>";
+            $i++;
+       }   
+    }
+    
+    
+    
+        //Displays the Total / AVG Time for Games Played
+    function displayTime()
+    {
+        global $startTime;
+        $timeInSeconds = microtime(true) - $startTime;
+        echo "<h3>This Game Took: " . $timeInSeconds . " in seconds (MicroTime) </h3><br /><br/>";
+        echo "<h3>Total Matches:"  . $_SESSION['gameCount'] . "</h3><br />";
+        $_SESSION['finalTime'] += $timeInSeconds;
+        echo "<h3>Total Time for All Matches: " .  $_SESSION['finalTime'] . "</h3><br /><br />";
+        echo "<h3>Average Time Per Game: " . ( $_SESSION['finalTime']  / $_SESSION['gameCount'])."</h3>";
+        $_SESSION['gameCount']++; 
+    } 
+    
+    //Display the winner base on a list of final score
+    function displayWinner($score, $players)
+    {
+        //print_r($score);
+        
+        //print_r($players);
+        
+        $totalScore = 0;
+        for ($i = 0; $i < count($score); $i++)
+        {
+            $totalScore += $score[$i];
+        }
+        $possible_winning_scores_array = array();
+        
+        for ($index=0; $index<4;$index++){
+            if($score[$index] <= 42){
+                array_push($possible_winning_scores_array,$score[$index]);
+            }
+        }
+        
+        $max_score =  (max($possible_winning_scores_array));
+        //echo $max_score;
+        $winning_index_array = array();
+        
+        $nbreOfWinners = 0;
+        $finalScore = $totalScore;
+        for($index = 0;$index<4;$index++){
+            if($score[$index]==$max_score)
+            {
+                $nbreOfWinners++;
+                $finalScore -= $max_score;
+                
+            }
+        }
+        for($index = 0;$index<4;$index++){
+            if($score[$index]==$max_score)
+            {
+                echo "</br><h1>".$players[$index]." WINS! With ". $finalScore ." Points!</h1>";
+            }
+        }
+    }
+    
+    // Play game function
+    function startSilverJack() 
+    {
+        $players = array("Evert", "Simrit", "Pierre", "Logan");           
+        $cards = array();
+        $FinalScore = array();
+        for($i=0; $i<52; $i++)
+        {
+            $cards[] = 0;
+        }
+        shuffle($cards);
+        shuffle($players);
+        
+        for($i = 0; $i < 4; $i++)
+        {
+            $player[$i] = array();
+            $playerScore = array();
+            //Display Player Image and Name
+            $player[$i] = retrievePlayer($players, $i);
+            $player[$i] = retrieveHand($cards);
+            //Display hand for the current Player
+            showPlayerCards($player[$i]);
+            //Get and display the Score of the curr Player
+	        $playerScore[$i] = $player[$i][count($player[$i])-1];
+	        
+	        array_push($FinalScore, $playerScore[$i]);
+	        echo '<span id="playerPoints" ><b>' . $playerScore[$i] . '</b></span> </br>';
+	        
+        }
+        
+        displayWinner($FinalScore, $players);
+        
+        echo '<br /><br />';
+        displayTime();
+    }
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <style type="text/css" href ="css/style.css" rel ="styles">
+    body, div  {
+           background-image:url("img/border.jpg");
+        }
+        h1 {
+            text-align: center;
+            color: white;
+            font-size: 4em;
+            background-color: black;
+        }
+        
+        h2 {
+            text-align: center;
+            padding: 150px;
+            color: black;
+            display: inline;
+            font-size: 2em;
+        }
+        
+        h3{
+            padding: 25px;
+            color: white;
+            display: inline;
+            font-size: 2em;
+        }
+        #players{
+            padding-left:35px;
+            padding-top:5px;
+        }
+        
+        #playerHand{
+            padding: 5px;
+        }
+        #playerPoints{
+            font-size: 50px;
+            color: white;
+            text-align: right;
+            padding-left: 55px;
+        }
+    
+    </style>
+    <head>
+        <title>Lab #3: SilverJack Card Game </title>
+    </head>
+    
+     <h1 style="color:powderblue;"> World Series of Poker</h1>
+     <h2 style="color:powderblue;"> Player: </h2>
+     <h2 style="color:powderblue;"> Cards:</h2>
+     <h2 style="color:powderblue;"> Score:</h2>
+     
+    <div>
+      <?php 
+      startSilverJack();
+      ?>
+      
+    </div>
+    
+    <body>
+
+    </body>
+    
+</html>
--- labs/lab4/index.php
+++ labs/lab4/index.php
@@ -1,3 +1,36 @@
+<?php
+if (isset($_GET['keyword'])) { //if form was submitted
+      
+      include 'api/pixabayAPI.php';
+      
+      $orientation = "horizontal";
+      $keyword = $_GET['keyword'];
+      
+      if(isset($_GET['layout'])){ //when checked a specific layout
+        $orientation = $_GET['layout'];
+        
+      }
+      
+      if(!empty($_GET['category'])){//when user selects a category
+        $keyword = $_GET['category'];
+      }
+      
+      $imageURLs = getImageURLs($keyword,$orientation);
+      
+      $backgroundImage = $imageURLs[array_rand($imageURLs)];
+      
+      
+  }     
+ 
+ function checkCategory($category){
+   
+    if ($category == $_GET['category']) {
+       echo " selected";
+    }
+ }
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,13 +38,78 @@
     </head>
     <style>
         @import url("https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
-        body{
-            background-image: url("img/sea.jpg");
+        @import url("css/styles.css");
+        body {
+            background-image: url(<?=$backgroundImage?>);
+        }
+        #carouselExampleIndicators{
+            width: 500px;
+            margin: 0 auto;/*centrs a div container*/
         }
     </style>
     <body>
+        
+        <form method="GET">
+            <input type="text" size ="5" name="keyword" placeholder="keyword" value="<?=$_GET['keyword']?>" />
+           
+            <input type="radio" name="layout" value="horizontal" id="hlayout" 
+            <?php
+            
+              if($_GET['layout']==horizontal){
+                echo "checked";
+              }
+            ?>>
+            <label for="hlayout"> Horizontal </label >
+            <input type="radio" name="layout" value="vertical" id="vlayout" <?=($_GET['layout']=="vertical")?'checked':''?>
+            >
+            <label for="vlayout"> Vertical </label>
+            
+            
+            <select name="category">
+              <option value="" >  Select One </option> 
+              <option value="sea" <?=checkCategory('sea')?>>  Ocean </option>
+              <option <?=checkCategory('Forest')?>>  Forest </option>
+              <option <?=checkCategory('Sky')?>>  Sky </option>
+            </select>
+            
+            <input type="submit" value="Submit"/>
+        </form>
+        
+        <?php
+            if(isset($_GET['keyword'])){
+        ?>
+        
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="<?=$imageURLs[0]?>" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="<?=$imageURLs[1]?>" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="<?=$imageURLs[2]?>" alt="Third slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+        
+        <?php echo "<h3>You searched for " . $_GET['keyword'] . "</h3>";} ?>
+        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" />
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" /></script>
     
     </body>
 </html>
